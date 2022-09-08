01b586507130c161ad68e3ea9c147ba2054624f6
dawolance <dawolance@gmail.com>
2022-09-08 14:59:59 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+ File  13 lines (12 sloc)  215 Bytes
+#include <stdio.h>
+#include <unistd.h>
+
+ /**
+  *  * main - Entry point
+  *   *
+  *    * Return: Always 0 (Success)
+  *     */
+ int main(void)
+{
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
